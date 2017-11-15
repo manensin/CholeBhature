d54d1205318e4bd8c5d6af14b1047c72c881dc6a
manender singh <manender singh@dell>
2017-11-15 21:55:16 +0530

    First Commit

---
+++ myProject/src/main/java/myProject/MainClass.java
@@ -0,0 +1,12 @@
+package myProject;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My First GIT Commit");
+		System.out.println("New COmmit");
+
+	}
+
+}
